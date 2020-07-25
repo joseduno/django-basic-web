a0eb947c8cd149c85261f0e285f66b365f9d4f1a
Jose Duno <jose.d2.v@gmail.com>
2020-07-25 22:20:37 +0000

    Modificado archivo settings.py para servir archivos estaticos en produccion

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^skny3e=qd_wra&d*kfs8jiu+d4fvw-&o#j8zz!@1kju1t!fds'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djangobasicweb.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files
 MEDIA_URL = '/media/'
